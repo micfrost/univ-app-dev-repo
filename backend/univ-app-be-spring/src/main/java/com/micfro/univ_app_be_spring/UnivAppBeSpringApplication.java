6cc8b273224f91f7003318860d8aeb52a70057c2
micfro <michalfrost71@gmail.com>
2025-06-18 14:07:34 +0200

    commit init

---
+++ backend/univ-app-be-spring/src/main/java/com/micfro/univ_app_be_spring/UnivAppBeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.micfro.univ_app_be_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnivAppBeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnivAppBeSpringApplication.class, args);
+	}
+
+}
---
+++ backend/univ-app-be-spring/src/test/java/com/micfro/univ_app_be_spring/UnivAppBeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micfro.univ_app_be_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnivAppBeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
